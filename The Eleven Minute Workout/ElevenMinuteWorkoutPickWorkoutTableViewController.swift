6a2774ecf63be7b5195d48e8e85723a1b379f343
Whitney Powell <whitney@whitneys-macbook-pro.local>
2016-03-23 13:16:22 -0400

    layout changes, additions to workout plist

---
+++ The Eleven Minute Workout/ElevenMinuteWorkoutPickWorkoutTableViewController.swift
@@ -0,0 +1,119 @@
+//
+//  ElevenMinuteWorkoutPickWorkoutTableViewController.swift
+//  The Elven Minute Workout
+//
+//  Created by Whitney Powell on 3/16/16.
+//  Copyright © 2016 Whitney Powell. All rights reserved.
+//
+
+import UIKit
+
+class ElevenMinuteWorkoutPickWorkoutTableViewController: UITableViewController {
+    private var selectedCell:(Int,Int)?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 2
+    }
+
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        if section == 0 {
+            return 6
+        } else {
+            return 2
+        }
+    }
+
+    
+//    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+//        let cell = tableView.dequeueReusableCellWithIdentifier("reuseIdentifier", forIndexPath: indexPath)
+//
+//        // Configure the cell...
+//
+//        return cell
+//    }
+    
+    override func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        selectedCell = (indexPath.section,indexPath.row)
+        performSegueWithIdentifier("startWorkoutSegue", sender: nil)
+    }
+    
+
+    /*
+    // Override to support conditional editing of the table view.
+    override func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool {
+        // Return false if you do not want the specified item to be editable.
+        return true
+    }
+    */
+
+    /*
+    // Override to support editing the table view.
+    override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
+        if editingStyle == .Delete {
+            // Delete the row from the data source
+            tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
+        } else if editingStyle == .Insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+        }    
+    }
+    */
+
+    /*
+    // Override to support rearranging the table view.
+    override func tableView(tableView: UITableView, moveRowAtIndexPath fromIndexPath: NSIndexPath, toIndexPath: NSIndexPath) {
+
+    }
+    */
+
+    /*
+    // Override to support conditional rearranging of the table view.
+    override func tableView(tableView: UITableView, canMoveRowAtIndexPath indexPath: NSIndexPath) -> Bool {
+        // Return false if you do not want the item to be re-orderable.
+        return true
+    }
+    */
+
+    
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+        if let identifier = segue.identifier {
+            switch identifier {
+            case "startWorkoutSegue":
+                if let countDownTimerController = segue.destinationViewController as? CountDownTimerViewController {
+                    if selectedCell?.0 == 0 {
+                        countDownTimerController.workoutIndex = selectedCell!.1 + 1
+                        countDownTimerController.currentExercise = 1
+                    }
+                }
+            default:
+                break
+            }
+
+        }
+    }
+    
+
+}
